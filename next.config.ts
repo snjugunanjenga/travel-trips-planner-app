4e427fbb9c1fd7c69aba03123913822dcd634504
Pedro Machado <machadop1407@gmail.com>
2025-06-18 17:47:03 -0700

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "ovco9b5jyh.ufs.sh",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
